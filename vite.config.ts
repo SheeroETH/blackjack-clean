4b0b7dfce5123e6ae0265a4c2b800bb29183703c
Theo Cousin <theocousin@Air-de-Theo.lan>
2025-03-26 19:38:35 +0800

    fix: specify index.html as entry in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+    outDir: 'dist'
+  }
 })
